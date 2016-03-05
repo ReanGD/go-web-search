5c70da12f581fcac48e123f208718684017d55cc
ReanGD <reangd@yandex.ru>
2016-03-05 18:31:24 +0300

    операция показа статистики по базе

--- crawler/cr_db_func.go
+++ crawler/cr_db_func.go
@@ -1,21 +1,19 @@
 package crawler
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+)
 
 type taskFromDB struct {
 	Host string
 	URL  string
 }
 
-func (db *DB) readNotLoadedURLs(baseHosts []string, cnt int, ch chan<- taskFromDB) error {
-	cntPerHost := cnt / len(baseHosts)
-	if cntPerHost < 1 {
-		cntPerHost = 1
-	}
-
-	leftCnts := make(map[string]int)
-	for _, host := range baseHosts {
-		leftCnts[host] = cntPerHost
+func (db *DB) readNotLoadedURLs(baseHosts map[string]int, ch chan<- taskFromDB) error {
+	leftCnts := make(map[string]int, len(baseHosts))
+	for k, v := range baseHosts {
+		leftCnts[k] = v
 	}
 
 	return db.View(func(tx *Tx) error {
@@ -51,17 +49,32 @@ func (db *DB) readNotLoadedURLs(baseHosts []string, cnt int, ch chan<- taskFromD
 
 		for host, cntLeft := range leftCnts {
 			if cntLeft > 0 {
-				baseURL := url.URL{Host: host, Scheme: "http"}
-				exists, err := b.Get([]byte(baseURL.String()), &urlVal)
+				u := URLFromHost(host)
+				exists, err := b.Get([]byte(u), &urlVal)
 				if err != nil {
 					return err
 				}
 				if !exists {
-					ch <- taskFromDB{Host: host, URL: baseURL.String()}
+					ch <- taskFromDB{Host: host, URL: u}
 				}
 			}
 		}
 
+		return nil
+	})
+}
+
+func (db *DB) showStatistics() error {
+	return db.View(func(tx *Tx) error {
+		bContents := tx.Bucket(DbBucketContents)
+		fmt.Printf("\nStatistics:\n")
+		fmt.Printf("Contents len = %d\n", bContents.Stats().KeyN)
+
+		bURLs := tx.Bucket(DbBucketURLs)
+		fmt.Printf("bURLs len = %d\n", bURLs.Stats().KeyN)
+
+		bWrongURLs := tx.Bucket(DbBucketWrongURLs)
+		fmt.Printf("WrongURLs len = %d\n", bWrongURLs.Stats().KeyN)
 		return nil
 	})
 }
